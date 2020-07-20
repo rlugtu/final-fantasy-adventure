5a157fe52120e1a28ddc6d66cc34f4535f391313
Ryan Lugtu <ryanlugtu@Ryans-MacBook-Pro.local>
2020-07-20 15:41:04 -0400

    added toggles for each screen

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+const mapScreen = document.querySelector(".map")
+const battleScreen = document.querySelector(".battle-screen")
+const shopScreen = document.querySelector(".shop")
+
+
+// MAP TOGGLES 
+const mapToggle = document.querySelector("#map-toggle")
+const battleToggle = document.querySelector('#battle-toggle')
+const shopToggle = document.querySelector('#shop-toggle')
+
+
+mapToggle.addEventListener('click', () => {
+    mapScreen.classList.remove('inactive')
+    battleScreen.classList.add('inactive')
+    shopScreen.classList.add('inactive')
+})
+
+battleToggle.addEventListener('click', () => {
+    mapScreen.classList.add('inactive')
+    battleScreen.classList.remove('inactive')
+    shopScreen.classList.add('inactive')
+})
+
+shopToggle.addEventListener('click', () => {
+    mapScreen.classList.add('inactive')
+    battleScreen.classList.add('inactive')
+    shopScreen.classList.remove('inactive')
+})
