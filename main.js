766877da0506761a96e678675f70bc629695678e
Ryan Lugtu <ryanlugtu@Ryans-MacBook-Pro.local>
2020-07-21 12:58:29 -0400

    added win condition to fight function

--- main.js
+++ main.js
@@ -10,6 +10,12 @@ const mapToggle = document.querySelector("#map-toggle")
 const battleToggle = document.querySelector('#battle-toggle')
 const shopToggle = document.querySelector('#shop-toggle')
 
+const switchToMap = () => {
+    mapScreen.classList.remove('inactive')
+    battleScreen.classList.add('inactive')
+    shopScreen.classList.add('inactive')
+}
+
 mapToggle.addEventListener('click', () => {
     mapScreen.classList.remove('inactive')
     battleScreen.classList.add('inactive')
@@ -57,44 +63,61 @@ window.addEventListener("keydown", (e) => {
 // Battle Options Event Listeners
 // have to move these inside a function when its the players turn 
 // attack button
-const attackOption = document.querySelector('#attack')
+const attackButton = document.querySelector('#attack')
 // attack animation class
 const attackAnimation = document.querySelector('.attack-animation')
+const attack = (enemy) => {
+    const isDead = (enemy) => {
+        if(enemy.health < 0) {
+            console.log('You won!')
+            setTimeout(function() {
+               switchToMap() 
+            }, 5000)
+        }
+    }
+    attackButton.addEventListener('click', () => {
+        setTimeout(function() {
+            attackAnimation.classList.remove('attack-off')
+        }, 1000)
+        
+        setTimeout(function() {
+            attackAnimation.classList.add('attack-off')
+        }, 3000) 
+        let playerIcon = document.querySelector('.player-icon')
+        setTimeout(function() {
+            playerIcon.classList.add('attackMove')
+        },300)
+        setTimeout(function() {
+            playerIcon.classList.remove('attackMove')
+        },1300)
 
-attackOption.addEventListener('click', () => {
-    setTimeout(function() {
-        attackAnimation.classList.remove('attack-off')
-    }, 1000)
-    
-    setTimeout(function() {
-        attackAnimation.classList.add('attack-off')
-    }, 3000) 
-    let playerIcon = document.querySelector('.player-icon')
-    setTimeout(function() {
-        playerIcon.classList.add('attackMove')
-    },300)
-    setTimeout(function() {
-        playerIcon.classList.remove('attackMove')
-    },1300)
-    
-})
+        enemy.health -= cloud.cloudAttack()
+        console.log('enemy health: ' + enemy.health)
+        isDead(enemy)
+    })
+}
+
 
 // item button
 const itemOption = document.querySelector('#item')
 let allOptions = document.querySelector('.player-options')
-itemOption.addEventListener('click', () => {
-    let inventory = document.querySelector('.inventory')
-    allOptions.classList.add('off')
-    inventory.classList.remove('off')
-})
-// open inventory after item button is clicked
-const backButton = document.querySelector('#backButton')
 
-backButton.addEventListener('click', () => {
-    let inventory = document.querySelector('.inventory')
-    allOptions.classList.remove('off')
-    inventory.classList.add('off')
-})
+const items = () => {
+    itemOption.addEventListener('click', () => {
+        let inventory = document.querySelector('.inventory')
+        allOptions.classList.add('off')
+        inventory.classList.remove('off')
+    })
+    // open inventory after item button is clicked
+    const backButton = document.querySelector('#backButton')
+    
+    backButton.addEventListener('click', () => {
+        let inventory = document.querySelector('.inventory')
+        allOptions.classList.remove('off')
+        inventory.classList.add('off')
+    })
+}
+
 // HEAL FUNCTION
 const healFunction = () => {
     let healAnimation = document.querySelector('.heal-animation')
@@ -113,33 +136,37 @@ healFunction()
 
 // steal button
 const stealOption = document.querySelector('#steal')
-stealOption.addEventListener('click', () => {
-    let playerIcon = document.querySelector('.player-icon')
-    setTimeout(function() {
-        playerIcon.classList.add('steal')
-    }, 1000)
-    setTimeout(function() {
-        playerIcon.classList.remove('steal')
-    }, 4000)
-    
-})
+const steal = () => {
+    stealOption.addEventListener('click', () => {
+        let playerIcon = document.querySelector('.player-icon')
+        setTimeout(function() {
+            playerIcon.classList.add('steal')
+        }, 1000)
+        setTimeout(function() {
+            playerIcon.classList.remove('steal')
+        }, 4000)
+    })
+}
+
 
 // run button
 const runOption = document.querySelector('#run')
-runOption.addEventListener('click', () => {
-    let playerIcon = document.querySelector('.player-icon')
-    setTimeout(function() {
-        playerIcon.classList.add('run')
-    },500)
-    setTimeout(function() {
-        playerIcon.classList.remove('run')
-    },4000)
-    setTimeout(function () {
-        mapScreen.classList.remove('inactive')
-        battleScreen.classList.add('inactive')
-        shopScreen.classList.add('inactive')
-    },3500)
-})
+const run = () => {
+    runOption.addEventListener('click', () => {
+        let playerIcon = document.querySelector('.player-icon')
+        setTimeout(function() {
+            playerIcon.classList.add('run')
+        },500)
+        setTimeout(function() {
+            playerIcon.classList.remove('run')
+        },4000)
+        setTimeout(function () {
+            mapScreen.classList.remove('inactive')
+            battleScreen.classList.add('inactive')
+            shopScreen.classList.add('inactive')
+        },3500)
+    })
+}
 
 
 // SHOP EVENT LISTENERS 
@@ -163,10 +190,36 @@ class Enemy {
         this.health = health;
         this.attack = attack;
     }
-    attackFunc() {
+    enemyAttack() {
         // return a random number from the range of damages the monster can make
-        return (this.attack[Math.floor(Math.random()*this.attack.length)])
+        return this.attack[Math.floor(Math.random()*this.attack.length)]
     }
 } 
 
-const monster1 = new Enemy('test', 100, [1,2,3,4,5]) 
+const monster1 = new Enemy('test', 10, [1,2,3,4,5]) 
+
+const cloud = {
+    name: 'Cloud',
+    attack: [1,2,3,4,5],
+    health: 100,
+
+    cloudAttack() {
+        return(this.attack[Math.floor(Math.random()*this.attack.length)])
+    }
+}
+
+// fight sequence
+const playerOptions = (enemy) => {
+    attack(enemy)
+    items()
+    steal()
+    run()
+}
+
+const fight = (enemy) => {
+    if(enemy.health > 0) {
+        playerOptions(enemy)
+    }
+}
+
+fight(monster1)
